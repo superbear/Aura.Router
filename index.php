12c04753ee308527e7a001ef32ef734734508919
superBear <superbear2013@gmail.com>
2014-01-13 20:01:37 +0800

    give some result for index.php

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ $router_map->add("home", "/");
 
 //add a simple unnamed route with params
 $router_map->add(null, '/{:controller}/{:action}/{:id:(\d+)}');
+/*url = /test/get/2
+ *it will return controller  test
+ *action get
+ *id  2
+ **/
 
 //add a complex name route
 $router_map->add("read","/blog/read/{:id}{:format}",[
@@ -20,6 +25,13 @@ $router_map->add("read","/blog/read/{:id}{:format}",[
         "format" => ".html",
     ]
     ]);
+/*url = /blog/read/2.html
+ *It will return controller blog
+ *action  read
+ *format .html
+ *id 2
+ **/
+
 $path = parse_url($_SERVER["PATH_INFO"], PHP_URL_PATH);
 $route = $router_map->match($path, $_SERVER);
 $params = $route->values;
